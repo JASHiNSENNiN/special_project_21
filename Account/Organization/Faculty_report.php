99921013102044e5f78452784cd6d1c74d2a11c9
Leahcim <kelaldea27@gmail.com>
2024-10-16 22:24:22 +0800

    lgo

--- Account/Organization/Faculty_report.php
+++ Account/Organization/Faculty_report.php
@@ -55,7 +55,6 @@ require_once 'show_profile.php';
                     <th>#</th>
                     <th>ID Picture</th>
                     <th>Student Name</th>
-                    <th>School</th>
                     <th>Result</th>
                     <th>Action</th>
 
@@ -65,7 +64,6 @@ require_once 'show_profile.php';
                     <td data-th="ID Picture"><img class="idpic" src="../Student/image/Default.png" alt="me">
                     </td>
                     <td data-th="Student Name">Joshua Rivera</td>
-                    <td data-th="School">Olscho</td>
                     <td data-th="Result">
 
                         <div class="container3">
@@ -84,7 +82,6 @@ require_once 'show_profile.php';
                     <td data-th="#">2</td>
                     <td data-th="ID Picture"><img class="idpic" src="../Student/image/Default.png" alt="me"></td>
                     <td data-th="Student Name">Dan Mamaid</td>
-                    <td data-th="School">Olscho</td>
                     <td data-th="Result">
                         <div class="container3">
                             <div class="circular-progress">
@@ -106,7 +103,7 @@ require_once 'show_profile.php';
 
 
     <script>
-        $("input:checkbox").on('click', function() {
+        $("input:checkbox").on('click', function () {
 
             var $box = $(this);
             if ($box.is(":checked")) {
@@ -138,7 +135,7 @@ require_once 'show_profile.php';
         let profilePic1 = document.getElementById("cover-pic");
         let inputFile1 = document.getElementById("input-file1");
 
-        inputFile1.onchange = function() {
+        inputFile1.onchange = function () {
             profilePic1.src = URL.createObjectURL(inputFile1.files[0]);
         }
     </script>
@@ -147,7 +144,7 @@ require_once 'show_profile.php';
         let profilePic2 = document.getElementById("profile-pic");
         let inputFile2 = document.getElementById("input-file2");
 
-        inputFile2.onchange = function() {
+        inputFile2.onchange = function () {
             profilePic2.src = URL.createObjectURL(inputFile2.files[0]);
         }
     </script>
