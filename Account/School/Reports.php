035694b11cd0bb79851dcc98dabd036ab92b80ce
Leahcim <kelaldea27@gmail.com>
2024-11-25 09:42:48 +0800

    expander

--- Account/School/Reports.php
+++ Account/School/Reports.php
@@ -57,27 +57,30 @@ require_once 'show_profile.php';
                     <td data-th="Company">NIA</td>
                     <td data-th="Student Name">Joshua Rivera</td>
                     <td data-th="Action">
-                        <!-- <a href="#" id="myBtn" class="btn-4d-can"><span>View</span></a> -->
-                        <button id="myBtn" class="button-9" role="button">View</button>
-                        <div id="myModal" class="modal">
-                            <!-- Modal content -->
-                            <div class="modal-content">
-                                <span class="close">&times;</span>
-                                <img src="image/evaluation.png" alt="" width="1100" height="1400">
-                            </div>
 
-                        </div><br>
+                        <button id="myBtn" class="button-9" onclick="toggleExpand(1)" role="button">View</button>
+
                         <button class="button-37" role="button">Archive</button>
                     </td>
                 </tr>
+                <tr id="expander-row-1" style="display:none;">
+                    <td colspan="4">
+                        <div class="expander-content">
+                            <p>This is the expanded content! It can be any HTML element, like text, images, etc.</p>
+                        </div>
+                    </td>
+                </tr>
+
                 <tr>
                     <td data-th="#">2</td>
                     <td data-th="Company">UPS South Inc.</td>
                     <td data-th="Student Name">Dan Mamaid</td>
-                    <td data-th="Action"><button id="myBtn" class="button-9" role="button">View</button>
+                    <td data-th="Action"><button id="myBtn" class="button-9" onclick="toggleExpand(2)"
+                            role="button">View</button>
                         <button class="button-37" role="button">Archive</button>
                     </td>
-                </tr>
+
+
                 <tr>
                     <td data-th="#">3</td>
                     <td data-th="Company">BOX Pro West</td>
@@ -109,50 +112,74 @@ require_once 'show_profile.php';
     </footer>
 
     <script>
-        let profilePic1 = document.getElementById("cover-pic");
-        let inputFile1 = document.getElementById("input-file1");
+    let profilePic1 = document.getElementById("cover-pic");
+    let inputFile1 = document.getElementById("input-file1");
 
-        inputFile1.onchange = function () {
-            profilePic1.src = URL.createObjectURL(inputFile1.files[0]);
-        }
+    inputFile1.onchange = function() {
+        profilePic1.src = URL.createObjectURL(inputFile1.files[0]);
+    }
     </script>
 
     <script>
-        let profilePic2 = document.getElementById("profile-pic");
-        let inputFile2 = document.getElementById("input-file2");
+    let profilePic2 = document.getElementById("profile-pic");
+    let inputFile2 = document.getElementById("input-file2");
 
-        inputFile2.onchange = function () {
-            profilePic2.src = URL.createObjectURL(inputFile2.files[0]);
-        }
+    inputFile2.onchange = function() {
+        profilePic2.src = URL.createObjectURL(inputFile2.files[0]);
+    }
     </script>
 
-    <script>
-        // Get the modal
-        var modal = document.getElementById("myModal");
+    <!-- <script>
+    // Get the modal
+    var modal = document.getElementById("myModal");
 
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
 
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-        // When the user clicks the button, open the modal 
-        btn.onclick = function () {
-            modal.style.display = "block";
-        }
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
 
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function () {
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
             modal.style.display = "none";
         }
+    }
+    </script> -->
 
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
+    <script>
+    // Toggle the display of the expanded content row
+    function toggleExpand(rowId) {
+        var expanderRow = document.getElementById('expander-row-' + rowId);
+        // Check the current display status and toggle it
+        if (expanderRow.style.display === "none" || expanderRow.style.display === "") {
+            expanderRow.style.display = "table-row";
+        } else {
+            expanderRow.style.display = "none";
         }
+    }
     </script>
+    <!-- <script>
+    function toggleExpand() {
+        const content = document.querySelector('.expander-content');
+        // Toggle visibility of the content
+        if (content.style.display === "none" || content.style.display === "") {
+            content.style.display = "block";
+        } else {
+            content.style.display = "none";
+        }
+    }
+    </script> -->
 
 
 </body>
