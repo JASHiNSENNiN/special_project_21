d44c3afb166096f23fe7c68d199c273c2e69f7c6
AsKm3wHy <nmiguelvon@gmail.com>
2025-05-18 14:14:31 +0800

    update

--- ProfileOrgView.php
+++ ProfileOrgView.php
@@ -332,7 +332,7 @@ $document_name_mapping = [
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
+    <!-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" /> -->
 
 </head>
 
